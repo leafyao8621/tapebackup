2797ef729f06b97fc9e16e087c8f4682fcb10069
Yi Yao <leaf_yao8621@hotmail.com>
2024-05-11 18:44:31 -0700

    Update

--- cli/src/util/util_dev.cpp
+++ cli/src/util/util_dev.cpp
@@ -36,9 +36,6 @@ bool TBCLI::Util::check_dev_write_protection(char *dev_name) {
     char all_one[64];
     char all_zero[64] = {0};
     memset(all_one, 0xff, 64);
-    for (size_t i = 0; i < 64; ++i) {
-        printf("%hhx\n", buf[i]);
-    }
     if (!memcmp(all_one, buf, 64)) {
         return true;
     }
