0a8bb4828e9a9a37a1db64bc5f0492b10ab9ac3d
Yi Yao <leaf_yao8621@hotmail.com>
2024-05-11 18:34:17 -0700

    Updates

--- cli/src/util/util_archive.cpp
+++ cli/src/util/util_archive.cpp
@@ -8,38 +8,34 @@ void TBCLI::Util::write_archive(char *dev_name) {
     std::ostringstream oss;
     oss << "mt -f " << dev_name << " rewind";
     system(oss.str().c_str());
-    char buf[128];
-    int dev = open(dev_name, O_RDWR);
-    if (dev == -1) {
-        throw DEVICE_OPEN;
-    }
-    read(dev, &buf, 128);
+    std::ostringstream oss_dd_skip;
+    oss_dd_skip <<
+        "dd of=" <<
+        getenv("HOME") << "/backup/backup.tar.gz" <<
+        " of=/dev/null status=progress";
+    system(oss_dd_skip.str().c_str());
+    system(oss_dd_skip.str().c_str());
     std::ostringstream oss_dd;
-    oss_dd << getenv("HOME") << "/backup/backup.tar.gz";
-    int archive = open(oss_dd.str().c_str(), O_RDONLY);
-    char wr_buf[1024];
-    int bytes_read = 0;
-    for (; (bytes_read = read(archive, wr_buf, 1024)) == 1024; ) {
-        write(dev, wr_buf, 1024);
-    }
-    if (bytes_read > 0) {
-        write(dev, wr_buf, bytes_read);
-    }
-    close(archive);
-    close(dev);
+    oss_dd <<
+        "dd if=" <<
+        getenv("HOME") << "/backup/backup.tar.gz" <<
+        " of=" <<
+        dev_name <<
+        " status=progress";
+    system(oss_dd.str().c_str());
 }
 
 void TBCLI::Util::read_archive(char *dev_name) {
     std::ostringstream oss;
     oss << "mt -f " << dev_name << " rewind";
     system(oss.str().c_str());
-    char buf[128];
-    int dev = open(dev_name, O_RDONLY);
-    if (dev == -1) {
-        throw DEVICE_OPEN;
-    }
-    read(dev, &buf, 128);
-    close(dev);
+    std::ostringstream oss_dd_skip;
+    oss_dd_skip <<
+        "dd of=" <<
+        getenv("HOME") << "/backup/backup.tar.gz" <<
+        " of=/dev/null status=progress";
+    system(oss_dd_skip.str().c_str());
+    system(oss_dd_skip.str().c_str());
     std::ostringstream oss_dd;
     oss_dd <<
         "dd if=" <<
