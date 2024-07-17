99654a1e8fb4aaf6e7c871ff8be5dbe09f4514b5
Yi Yao <leaf_yao8621@hotmail.com>
2024-07-17 12:45:25 -0700

    Updates

--- python/script/test.py
+++ python/script/test.py
@@ -2,5 +2,5 @@ import tapebackup
 
 if __name__ == "__main__":
     tapebackup.write("/home/leaf/test", "/home/leaf/dummy.img", verbose=True)
-    tapebackup.read("/home/leaf/test", "/home/leaf/dummy.img", verbose=True)
+    tapebackup.read(".", "/home/leaf/dummy.img", verbose=True)
     tapebackup.check("/home/leaf/dummy.img", verbose=True)
